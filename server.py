604ed8d4c977ba1a236b0f679a78b35e975e2f4e
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-02-11 19:49:30 +0000

    Bringing back the random outside of the function

--- server.py
+++ server.py
@@ -3,11 +3,11 @@ import random
 
 app = Flask(__name__)
 
-
+random_number = random.randint(0,9) 
+print(random_number)
 @app.route("/")
 def home_route():
-    random_number = random.randint(0,9) 
-    print(random_number)
+
     return ('<h1>Guess a number between 0 and 9</h1>'
             '<img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWYxcTFmOTd5bHJjdThwZzFnOHE1YzRxZTA1bzExYXNqYWN6eTgwaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8rN9VXNb7dfU792YQt/giphy.gif"" width=200>'
 
